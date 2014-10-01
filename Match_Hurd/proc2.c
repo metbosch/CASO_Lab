54995b9a4e010ed8edc3359bebdd7b2a6d270082
root <root@debian>
2014-10-01 05:55:00 -0400

    more shit inside proc2.c

--- Match_Hurd/proc2.c
+++ Match_Hurd/proc2.c
@@ -2,7 +2,7 @@
 #include <mach_error.h>
 #include <mach/mig_errors.h>
 #include <mach/thread_status.h>
-#include <mach/processor_info.h>>
+#include <mach/processor_info.h>
 #include <mach/mach_host.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,27 +11,33 @@
         processor_array_t processor_list = NULL;
         mach_msg_type_number_t processor_listCnt = 0;
 
-void printProcInfo ( processor_t proc, unsigned int index ) {
+void printProcInfo ( processor_t proc, unsigned int index, mach_port_t* host ) {
+   printf("olaaa2");
    mach_msg_type_number_t procInfoCont;
-   kernel_return_t ret;
+   int ret;
    processor_info_t info;
    processor_basic_info_t basicInfo;
-
+   printf("....olaaaa...");
+   fflush(stdout);   
    procInfoCont = sizeof(int)*PROCESSOR_INFO_MAX;
    ret = processor_info (proc, PROCESSOR_BASIC_INFO, host, info, &procInfoCont);
-   if( res != KERN_SUCCESS) {
-	   fprintf(stderr, "Error getting the processor %d info [0x%x]:\n\t%s\n", i, res, mach_error_string(res));
-	   exit(1);
+   printf("%i",ret);
+   fflush(stdout);
+   if( ret != KERN_SUCCESS) {
+	  printf("Error getting the processor %d info [0x%x]:\n\t%s\n", index, ret, mach_error_string(ret));
+	fflush(stdout);   
+	exit(1);
    }
 
    basicInfo = (processor_basic_info_t) &info[index];
-   fprintf(stdout, "------------> CPU %d <------------\n", index);
-   fprintf(stdout, "CPU Type:   \t\t%d\n", basicInfo->cpu_type);
-   fprintf(stdout, "CPU Subtype:\t\t%d\n", basicInfo->cpu_subtype);
-   fprintf(stdout, "CPU Running:\t\t%s\n", basicInfo->running ? "YES" : "NO");
-   fprintf(stdout, "Slot number:\t\t%d\n", basicInfo->slot_num);
-   fprintf(stdout, "CPU Master: \t\t%s\n", basicInfo->is_master ? "YES" : "NO");
-   fprintf(stdout, "----------------------------------\n");
+   printf("------------> CPU %d <------------\n", index);
+   printf("CPU Type:   \t\t%d\n", basicInfo->cpu_type);
+   printf("CPU Subtype:\t\t%d\n", basicInfo->cpu_subtype);
+   printf("CPU Running:\t\t%s\n", basicInfo->running ? "YES" : "NO");
+   printf("Slot number:\t\t%d\n", basicInfo->slot_num);
+   printf("CPU Master: \t\t%s\n", basicInfo->is_master ? "YES" : "NO");
+   printf("----------------------------------\n");
+   fflush(stdout);
 }
 
 int main ()
@@ -62,7 +68,10 @@ int main ()
 
    printf ("        processors at array 0x%x\n", processor_list);
    printf ("processor_listCnt %d\n", processor_listCnt);
-
-   for (i=0; i < processor_listCnt; i++)
-      printProcInfo(processor_list[i], i);
+   
+   for (i=0; i < processor_listCnt; i++) {
+      printf("HOLALLALAL\n");
+      fflush(stdout);
+      printProcInfo(processor_list[i], i, &host_privileged_port);
+   }
 }
