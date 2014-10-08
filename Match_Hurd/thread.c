e7afe10879c7186194a28149393206b8e4e6d6d0
metbosch <boschpons@gmail.com>
2014-10-08 09:51:14 +0200

    Implemented thread exercise

--- Match_Hurd/thread.c
+++ Match_Hurd/thread.c
@@ -8,9 +8,20 @@
 #include <stdlib.h>
 #include <hurd.h>
 
+#define STACK_SIZE 8192
+#define SLEEP_TIME 15
+
 
         processor_array_t processor_list = NULL;
         mach_msg_type_number_t processor_listCnt = 0;
+
+unsigned char threadStack[STACK_SIZE];
+
+void threadLoop() {
+   while(1) {
+
+   }
+}
 
 void checkError(int res, char * str) {
    if (res != KERN_SUCCESS) {
@@ -21,9 +32,22 @@ void checkError(int res, char * str) {
 
 int main ()
 {
-   int res, i;
+   i386_thread_state thread_state;
+   mach_msg_type_number_t state_count;
    mach_port_t child;
    mach_port_t self = mach_task_self();
 
    checkError(thread_create(self, &child), "Error creating thread (0x%x), %s\n");
+   checkError(thread_get_state(child, i386_THREAD_STATE, &thread_state, &state_count),
+              "Error getting thread state (0x%x), %s\n");
+   thread_state.uesp = threadStack[STACK_SIZE] - 100;
+   thread_state.eip = threadLoop;
+   checkError(thread_set_state(child, i386_THREAD_STATE, thread_state, state_count),
+              "Error setting thread state (0x%x), %s\n");
+   checkError(thread_resume(child), "Error resuming thread (0x%x), %s\n");
+
+   sleep(SLEEP_TIME);
+
+   checkError(thread_terminate(child), "Error terminating thread (0x%x), %s\n");
+
 }
