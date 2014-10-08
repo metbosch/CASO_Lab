75306d6935db4c608f05faf3c6f7435e042a42ce
metbosch <boschpons@gmail.com>
2014-10-08 09:00:39 +0200

    First version of my ps

---
+++ Match_Hurd/mps.c
@@ -0,0 +1,53 @@
+#include <mach.h>
+#include <mach_error.h>
+#include <mach/mig_errors.h>
+#include <mach/thread_status.h>
+#include <mach/processor_info.h>
+#include <mach/mach_host.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <hurd.h>
+#include "print-task-info.c"
+
+        processor_array_t processor_list = NULL;
+        mach_msg_type_number_t processor_listCnt = 0;
+
+int main ()
+{
+   int res, i;
+   mach_port_t host_privileged_port, pset, pset_priv;
+   task_array_t* task_list;
+   match_msg_type_number_t* task_count;
+
+   res = get_privileged_ports(&host_privileged_port, &device_privileged_port);
+   if (res != KERN_SUCCESS) {
+      printf ("Error getting privileged ports (0x%x), %s\n", res,
+                mach_error_string(res));
+      exit(1);
+   }
+
+   res = processor_set_default(host_privileged_port, &pset);
+   if (res != KERN_SUCCESS) {
+      printf ("Error setting default processor (0x%x), %s\n", res,
+                mach_error_string(res));
+      exit(1);
+   }
+
+   res = host_processor_set_priv(host_privileged_port, p_set, &pset_priv);
+   if (res != KERN_SUCCESS) {
+      printf ("Error setting privileged processor (0x%x), %s\n", res,
+                mach_error_string(res));
+      exit(1);
+   }
+
+   res = processor_set_tasks(host_privileged_port, task_list, task_count);
+   if (res != KERN_SUCCESS) {
+      printf ("Error getting set of tasks (0x%x), %s\n", res,
+                mach_error_string(res));
+      exit(1);
+   }
+
+   for (i=0; i < (int)(*task_count); i++) {
+      Print_Task_info(task_list[i]);
+   }
+}
