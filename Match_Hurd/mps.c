10369c63f13a543b95708a639904af1f50358109
root <root@debian>
2014-10-08 03:13:58 -0400

    mps working

--- Match_Hurd/mps.c
+++ Match_Hurd/mps.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <hurd.h>
-#include "print-task-info.c"
+
 
         processor_array_t processor_list = NULL;
         mach_msg_type_number_t processor_listCnt = 0;
@@ -17,8 +17,8 @@ int main ()
    int res, i;
    mach_port_t host_privileged_port, pset, pset_priv;
    device_t device_privileged_port;
-   task_array_t* task_list;
-   mach_msg_type_number_t* task_count;
+   task_array_t task_list;
+   mach_msg_type_number_t task_count;
 
    res = get_privileged_ports(&host_privileged_port, &device_privileged_port);
    if (res != KERN_SUCCESS) {
@@ -41,14 +41,14 @@ int main ()
       exit(1);
    }
 
-   res = processor_set_tasks(host_privileged_port, task_list, task_count);
+   res = processor_set_tasks(pset_priv, &task_list, &task_count);
    if (res != KERN_SUCCESS) {
       printf ("Error getting set of tasks (0x%x), %s\n", res,
                 mach_error_string(res));
       exit(1);
    }
 
-   for (i=0; i < (int)(*task_count); i++) {
+   for (i=0; i < (int)(task_count); i++) {
       Print_Task_info((task_t)task_list[i]);
    }
 }
