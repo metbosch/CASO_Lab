a909d9629c03e4c353f00a190ba5f7fe1bb445a5
metbosch <boschpons@gmail.com>
2014-10-15 09:20:52 +0200

    Added vm-read.c exercicie

---
+++ Match_Hurd/vm-read.c
@@ -0,0 +1,46 @@
+#include <mach.h>
+#include <mach_error.h>
+#include <mach/mig_errors.h>
+#include <mach/thread_status.h>
+#include <mach/processor_info.h>
+#include <mach/mach_host.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <hurd.h>
+#include <unistd.h>
+
+#define NUM_PARAMS 2
+#define READ_SIZE 16
+
+void checkError(int res, char * str) {
+   if (res != KERN_SUCCESS) {
+      printf (str, res, mach_error_string(res));
+      exit(1);
+   }
+}
+
+void usage() {
+   printf("Usage: PID, mem dir\n");
+   exit(1);
+}
+
+int main (int argc, char *argc[])
+{
+   unsigned int address, pid, i;
+   match_port_t task;
+   mach_msg_type_number_t data_count;
+   char data[READ_SIZE];
+
+   if (argc != NUM_PARAMS + 1) usage();
+   pid = atoi(argc[1]);
+   address = (vm_address_t)(atoi(argc[2]));
+
+   task = (match_port_t)(pid2task((pid_t)pid));
+   checkError(task == MACH_PORT_NULL, "Error getting the task from PID (0x%x), %s\n");
+   checkError(vm_read(task, (vm_address_t)address, (vm_offset_t *)(&data), data_count),
+              "Error reading (0x%x), %s\n");
+   checkError((unsigned int)(data_count) != READ_SIZE, "Error num bytes readed (expected 16 bytes)");
+
+   write(1, &data, PAGE_SIZE);
+
+}
