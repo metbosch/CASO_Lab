73cf317d243f8b2daa403eeb346866f50ae0ce1a
Marc Cobos <coboshernandez@gmail.com>
2014-11-05 08:39:24 +0100

    report done

--- Mesures_Rendiment/read-and-write.sh
+++ Mesures_Rendiment/read-and-write.sh
@@ -4,6 +4,7 @@ if [ "$#" -ne 4 ]; then
     echo "Usage: chunkSize, sync[0, 1], FilePathRead, FilePathWrite"
     exit
 fi
+#sync; sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 echo "./write-to-disk2 $1 $2 $4 &"
 ./write-to-disk2 $1 $2 $4 &
 echo "./read-from-disk $1 $2 $3 &"
